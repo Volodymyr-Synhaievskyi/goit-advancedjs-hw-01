86810cb2477e57a6d8a8f9495b8a11b7e9483cf9
Volodymyr-Synhaievskyi <vovagidro@gmail.com>
2024-01-23 14:25:25 +0100

    23.01

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash/throttle';
 
+const player = new Vimeo(document.getElementById('vimeo-player'));
+
+const saveCurrentTime = throttle(() => {
+  player.getCurrentTime().then(currentTime => {
+    localStorage.setItem('videoplayer-current-time', currentTime);
+  });
+}, 1000);
+player.on('timeupdate', saveCurrentTime);
+
+document.addEventListener('DOMContentLoaded', () => {
+  const savedTime = localStorage.getItem('videoplayer-current-time');
+  if (savedTime) {
+    player.setCurrentTime(parseFloat(savedTime));
+  }
+});
